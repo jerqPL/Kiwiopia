582b66d29391619a8b7dc1ca0e60e1f15efd06a6
jerqPL <wiktor.karol@outlook.com>
2026-01-19 20:29:38 +0100

    current menu state

---
+++ Assets/_Scripts/UI/Main Menu/TitleAnimation.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TitleAnimation : MonoBehaviour
+{
+    [SerializeField] private float minRotation = 170;
+    [SerializeField] private float maxRotation = 190;
+    [SerializeField] private float cycleDuration = 10f;
+
+    private float currentRotation;
+
+    private InputActionMap selectionMap;
+    private InputAction mousePosition;
+
+    void Start()
+    {
+        selectionMap = InputSystem.actions.FindActionMap("Selection");
+        mousePosition = selectionMap.FindAction("Mouse Position");
+
+        Vector2 mousePos = mousePosition.ReadValue<Vector2>();
+
+
+        currentRotation = Mathf.Lerp(maxRotation, minRotation, mousePos.x / Screen.width);
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,
+                                              currentRotation,
+                                              transform.rotation.eulerAngles.z);
+    }
+
+
+    void Update()
+    {
+        Vector2 mousePos = mousePosition.ReadValue<Vector2>();
+
+
+        currentRotation = Mathf.Lerp(maxRotation, minRotation, mousePos.x / Screen.width);
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,
+                                              currentRotation,
+                                              transform.rotation.eulerAngles.z);
+    }
+}
